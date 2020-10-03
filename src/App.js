9d6a1cc3e9401f16befbef472998693a81c1115f
Jose Hernandez <josemanuelhdez2@gmail.com>
2020-10-03 13:20:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Ludum Dare 47</h1>
+      <h2>Stuck In A Loop</h2>
+    </div>
+  );
+}
